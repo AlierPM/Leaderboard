ecbae8328a22b7110cecd54a09045857b7c6bb77
Alier Philip <alierditmaguet@gmail.com>
2022-09-02 16:26:54 +0200

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 import './style.css';
-import Store from './modules/store.js';
-import UI from './modules/ui.js';
-import User from './modules/user.js';
+import sendNewScore from './modules/store.js';
+import getData from './modules/get.js';
 
-document.addEventListener('DOMContentLoaded', UI.displayUsers);
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/j22uKXxrSkg52SSFn2tq/scores';
+
+document.querySelector('.refresh-btn').addEventListener('click', () => {
+  window.location.reload();
+});
 
 const form = document.querySelector('#user-form');
+const submitButton = document.getElementsByClassName('add-btn');
 
-form.addEventListener('submit', (e) => {
+
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const name = document.querySelector('#name').value;
-  const score = document.querySelector('#score').value;
+  const userName = document.querySelector('#name').value;
+  const userScore = document.querySelector('#score').value;
 
-  const user = new User(name, score);
+  const data = {
+    user: userName,
+    score: userScore,
+  };
 
-  Store.addUser(user);
-  UI.addUserToList(user);
+  sendNewScore(url, data);
 
-  UI.clearFields();
+  document.querySelector('#name').value = '';
+  document.querySelector('#score').value = '';
+});
+getData(url);
+
+submitButton.addEventListener('click', async () => {
+  const nameInput = document.getElementById('name');
+  const scoreInput = document.getElementById('score');
+
+  if (nameInput.value !== '' && scoreInput.value !== '') {
+    createNewScore(url, nameInput.value, scoreInput.value);
+    nameInput.value = '';
+    scoreInput.value = '';
+  }
 });
